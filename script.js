8163e6cddfaba656fb98f1fc655383ed195a8897
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-02-19 18:54:25 -0800

    Created 4 basic JS operator functions to perform basic mathematics.

---
+++ script.js
@@ -0,0 +1,12 @@
+/* -- OPERATOR FUNCTIONS -- */
+
+let add = (num1, num2) => num1 + num2;
+let subtract = (num1, num2) => num1 - num2;
+let multiply = (num1, num2) => num1 * num2;
+let divide = (num1, num2) => num1 / num2;
+
+
+console.log(divide(36, 3));
+console.log(multiply(12, 5));
+console.log(subtract(120, 20));
+console.log(add(66, 3));
